6407fc4d373c724e929dbc356e0751cf025445ce
kevin <setiawank417@gmail.com>
2022-08-16 17:00:13 +0700

    pus daftar tamu

--- resources/views/Daftar-tamu.blade.php
+++ resources/views/Daftar-tamu.blade.php
@@ -1,6 +1,5 @@
 @extends('Layouts.Main')
 <div class="page-wrapper">
-    <h5>Daftar Tamu</h5>
     <div class="container-fluid">
         <div class="row heading-bg">
             @include('Layouts.Breadcrumb')
@@ -8,18 +7,40 @@
                 <div class="col-sm-12">
                     <div class="panel panel-default card-view">
                         <div class="panel-heading">
-                            <div class="pull-left">
-                                <h6 class="panel-title txt-dark">Daftar Tamu</h6>
-                            </div>
-                            <div class="clearfix"></div>
-                            <form action="#" style="text-align: end;">
-                                <i class="zmdi zmdi-fullscreen"></i>
-                                <i class="fa fa-plus"></i>
-                                <div id="search" style="justify-content: right">
-                                    <input type="text" id="input" placeholder="Search">
-                                    <button id="button"><i class="fa fa-search"></i></button>
+
+                            {{-- <div class="clearfix"></div> --}}
+                            <div class="row">
+                                <div class="col-lg-10">
+                                    <h6>Daftar Tamu</h6>
                                 </div>
-                            </form>
+                                <div class="col-lg-2" style="text-align: end;">
+                                    <i class="fa-2x zmdi zmdi-fullscreen"
+                                        style="border: 0px solid silver; border-radius: 0.25em; padding: 0.5em;"></i>
+                                    <i class="fa-2x fa-plus"
+                                        style="border: 0px solid silver; border-radius: 0.25em; padding: 0.5em;"></i>
+
+                                    <div class="row">
+                                        <div class="col-lg-0"></div>
+                                        <div class="col-lg-4">
+                                            <div class="boxcontainer">
+                                                <table class="elementcontainer">
+                                                    <tr>
+                                                        <td>
+                                                            <input type="text" placeholder="search" class="search">
+                                                        </td>
+                                                        <td>
+                                                            <a href="#"><i
+                                                                    class="fa-solid fa-magnifying-glass"></i></a>
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                </div>
+                            </div>
                         </div>
                         <div class="panel-wrapper collapse in">
                             <div class="panel-body">
@@ -28,91 +49,121 @@
                                         <table class="table mb-0">
                                             <thead>
                                                 <tr>
-                                                    <th>ID</th>
-                                                    <th>Nama</th>
-                                                    <th>Email</th>
-                                                    <th>Nomer hp</th>
-                                                    <th>Kategori Tamu</th>
-                                                    <th>Aksi</th>
+                                                    <th style="text-align: center;">ID</th>
+                                                    <th style="text-align: center;">Nama</th>
+                                                    <th style="text-align: center;">Email</th>
+                                                    <th style="text-align: center;">Nomer hp</th>
+                                                    <th style="text-align: center;">Kategori Tamu</th>
+                                                    <th style="text-align: center;">Aksi</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
                                                 <tr>
-                                                    <td>#13324</td>
-                                                    <td>Pamungkas Nuli Ramadhan</td>
-                                                    <td>pamungkasnuli@rocketmail.com</td>
-                                                    <td>081126458792</td>
-                                                    <td><span class="label label-vip">VIP</span> </td>
-                                                    <td><i class="fa fa-qrcode"></i>
-                                                        <i class="fa fa-file-pen"></i>
-                                                        <i data-fa-symbol="delete" class="fas fa-trash fa-fw"></i>
-                                                        <i class="fa fa-square-dashed"></i>
+                                                    <td style="text-align: center;">#13324</td>
+                                                    <td style="text-align: center;">Pamungkas Nuli Ramadhan</td>
+                                                    <td style="text-align: center;">pamungkasnuli@rocketmail.com</td>
+                                                    <td style="text-align: center;">081126458792</td>
+                                                    <td style="text-align: center;"><span
+                                                            class="label label-vip">VIP</span> </td>
+                                                    <td style="text-align: center;">
+                                                        <img src="dist/img/Card-Tamu.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
+                                                        <img src="dist/img/edit.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
+                                                        <img src="dist/img/hapus.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
                                                     </td>
                                                 </tr>
                                                 <tr>
-                                                    <td>#13324</td>
-                                                    <td>Pamungkas Nuli Ramadhan</td>
-                                                    <td>pamungkasnuli@rocketmail.com</td>
-                                                    <td>081126458792</td>
-                                                    <td><span class="label label-vip">VIP</span> </td>
-                                                    <td> <i class="fa fa-qrcode"></i>
-                                                        <i class="fa fa-file-pen"></i>
-                                                        <i data-fa-symbol="delete" class="fas fa-trash fa-fw"></i>
+                                                    <td style="text-align: center;">#13324</td>
+                                                    <td style="text-align: center;">Pamungkas Nuli Ramadhan</td>
+                                                    <td style="text-align: center;">pamungkasnuli@rocketmail.com</td>
+                                                    <td style="text-align: center;">081126458792</td>
+                                                    <td style="text-align: center;"><span
+                                                            class="label label-vip">VIP</span> </td>
+                                                    <td style="text-align: center;"> <img src="dist/img/Card-Tamu.svg"
+                                                            alt="" style="padding: 2px 7px 2px 2px;">
+                                                        <img src="dist/img/edit.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
+                                                        <img src="dist/img/hapus.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
                                                     </td>
                                                 </tr>
                                                 <tr>
-                                                    <td>#13324</td>
-                                                    <td>Pamungkas Nuli Ramadhan</td>
-                                                    <td>pamungkasnuli@rocketmail.com</td>
-                                                    <td>081126458792</td>
-                                                    <td><span class="label label-vip">VIP</span> </td>
-                                                    <td> <i class="fa fa-qrcode"></i>
-                                                        <i class="fa fa-file-pen"></i>
-                                                        <i data-fa-symbol="delete" class="fas fa-trash fa-fw"></i>
+                                                    <td style="text-align: center;">#13324</td>
+                                                    <td style="text-align: center;">Pamungkas Nuli Ramadhan</td>
+                                                    <td style="text-align: center;">pamungkasnuli@rocketmail.com</td>
+                                                    <td style="text-align: center;">081126458792</td>
+                                                    <td style="text-align: center;"><span
+                                                            class="label label-vip">VIP</span> </td>
+                                                    <td style="text-align: center;"> <img src="dist/img/Card-Tamu.svg"
+                                                            alt="" style="padding: 2px 7px 2px 2px;">
+                                                        <img src="dist/img/edit.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
+                                                        <img src="dist/img/hapus.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
                                                     </td>
                                                 </tr>
                                                 <tr>
-                                                    <td>#13324</td>
-                                                    <td>Pamungkas Nuli Ramadhan</td>
-                                                    <td>pamungkasnuli@rocketmail.com</td>
-                                                    <td>081126458792</td>
-                                                    <td><span class="label label-vip">VIP</span> </td>
-                                                    <td> <i class="fa fa-qrcode"></i>
-                                                        <i class="fa fa-file-pen"></i>
-                                                        <i data-fa-symbol="delete" class="fas fa-trash fa-fw"></i>
+                                                    <td style="text-align: center;">#13324</td>
+                                                    <td style="text-align: center;">Pamungkas Nuli Ramadhan</td>
+                                                    <td style="text-align: center;">pamungkasnuli@rocketmail.com</td>
+                                                    <td style="text-align: center;">081126458792</td>
+                                                    <td style="text-align: center;"><span
+                                                            class="label label-vip">VIP</span> </td>
+                                                    <td style="text-align: center;"> <img src="dist/img/Card-Tamu.svg"
+                                                            alt="" style="padding: 2px 7px 2px 2px;">
+                                                        <img src="dist/img/edit.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
+                                                        <img src="dist/img/hapus.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
                                                     </td>
                                                 </tr>
                                                 <tr>
-                                                    <td>#13324</td>
-                                                    <td>Pamungkas Nuli Ramadhan</td>
-                                                    <td>pamungkasnuli@rocketmail.com</td>
-                                                    <td>081126458792</td>
-                                                    <td><span class="label label-vvip">VVIP</span> </td>
-                                                    <td> <i class="fa fa-qrcode"></i>
-                                                        <i class="fa fa-file-pen"></i>
-                                                        <i data-fa-symbol="delete" class="fas fa-trash fa-fw"></i>
+                                                    <td style="text-align: center;">#13324</td>
+                                                    <td style="text-align: center;">Pamungkas Nuli Ramadhan</td>
+                                                    <td style="text-align: center;">pamungkasnuli@rocketmail.com</td>
+                                                    <td style="text-align: center;">081126458792</td>
+                                                    <td style="text-align: center;"><span
+                                                            class="label label-vvip">VVIP</span> </td>
+                                                    <td style="text-align: center;">
+                                                        <img src="dist/img/Card-Tamu.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
+                                                        <img src="dist/img/edit.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
+                                                        <img src="dist/img/hapus.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
                                                     </td>
                                                 </tr>
                                                 <tr>
-                                                    <td>#13324</td>
-                                                    <td>Pamungkas Nuli Ramadhan</td>
-                                                    <td>pamungkasnuli@rocketmail.com</td>
-                                                    <td>081126458792</td>
-                                                    <td><span class="label label-vvip">VVIP</span> </td>
-                                                    <td> <i class="fa fa-qrcode"></i>
-                                                        <i class="fa fa-file-pen"></i>
-                                                        <i data-fa-symbol="delete" class="fas fa-trash fa-fw"></i>
+                                                    <td style="text-align: center;">#13324</td>
+                                                    <td style="text-align: center;">Pamungkas Nuli Ramadhan</td>
+                                                    <td style="text-align: center;">pamungkasnuli@rocketmail.com</td>
+                                                    <td style="text-align: center;">081126458792</td>
+                                                    <td style="text-align: center;"><span
+                                                            class="label label-vvip">VVIP</span> </td>
+                                                    <td style="text-align: center;">
+                                                        <img src="dist/img/Card-Tamu.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
+                                                        <img src="dist/img/edit.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
+                                                        <img src="dist/img/hapus.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
                                                     </td>
                                                 </tr>
                                                 <tr>
-                                                    <td>#13324</td>
-                                                    <td>Pamungkas Nuli Ramadhan</td>
-                                                    <td>pamungkasnuli@rocketmail.com</td>
-                                                    <td>081126458792</td>
-                                                    <td><span class="label label-vip">VIP</span> </td>
-                                                    <td> <i class="fa fa-qrcode"></i>
-                                                        <i class="fa fa-file-pen"></i>
-                                                        <i data-fa-symbol="delete" class="fas fa-trash fa-fw"></i>
+                                                    <td style="text-align: center;">#13324</td>
+                                                    <td style="text-align: center;">Pamungkas Nuli Ramadhan</td>
+                                                    <td style="text-align: center;">pamungkasnuli@rocketmail.com</td>
+                                                    <td style="text-align: center;">081126458792</td>
+                                                    <td style="text-align: center;"><span
+                                                            class="label label-vvip">VVIP</span> </td>
+                                                    <td style="text-align: center;"> <img src="dist/img/Card-Tamu.svg"
+                                                            alt="" style="padding: 2px 7px 2px 2px;">
+                                                        <img src="dist/img/edit.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
+                                                        <img src="dist/img/hapus.svg" alt=""
+                                                            style="padding: 2px 7px 2px 2px;">
                                                     </td>
                                                 </tr>
                                             </tbody>
