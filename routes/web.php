8929619fe8ef86ec9956b8a3bf5046469a6be05e
kevin <setiawank417@gmail.com>
2022-08-19 15:18:04 +0700

    update daftar-tamu

--- routes/web.php
+++ routes/web.php
@@ -41,12 +41,14 @@ Route::get('/logout', [AuthController::class, 'logout']);
 Route::group(['middleware' => ['auth','ceklevel:1']], function() {
 Route::get('/dashboard', [AuthController::class, 'dashboard'])->middleware('auth');
 Route::get('/daftar-admin', [AuthController::class, 'daftar_admin'])->name('daftar-admin');
+Route::get('/tambah-admin', [AuthController::class, 'tambah_admin'])->name('tambah-admin');
 });
 
 
 Route::group(['middleware' => ['auth','ceklevel:1']], function() {
 Route::get('/dashboard', [AuthController::class, 'dashboard'])->middleware('auth');
 Route::get('/daftar-admin', [AuthController::class, 'daftar_admin'])->name('daftar-admin');
+Route::get('/tambah-admin', [AuthController::class, 'tambah_admin'])->name('tambah-admin');
 
 });
 
