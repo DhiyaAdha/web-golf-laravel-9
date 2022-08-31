9e7f6d27750e9c70f9079db0ec5ed706b49b61c7
yudistira-rk <yudistiraeducation100@gmail.com>
2022-08-31 16:19:09 +0700

    31/08/2022

--- resources/views/Invoice/metode-pembayaran.blade.php
+++ resources/views/Invoice/metode-pembayaran.blade.php
@@ -79,29 +79,6 @@
                                         <option>Cash / Transfer</option>
                                         <option>Deposit</option>
                                     </select>
-                                    <div class="form-group">
-                                        <label class="control-label mb-10">Value set 30 </label>
-                                        <div class="input-group bootstrap-touchspin"><span class="input-group-btn"><button
-                                                    class="btn btn-default bootstrap-touchspin-down"
-                                                    type="button">-</button></span><span
-                                                class="input-group-addon bootstrap-touchspin-prefix"
-                                                style="display: none;"></span><input id="tch3_22" type="text"
-                                                value="30" name="tch3_22" data-bts-button-down-class="btn btn-default"
-                                                data-bts-button-up-class="btn btn-default" class="form-control"
-                                                style="display: block;"><span
-                                                class="input-group-addon bootstrap-touchspin-postfix"
-                                                style="display: none;"></span><span class="input-group-btn"><button
-                                                    class="btn btn-default bootstrap-touchspin-up"
-                                                    type="button">+</button></span></div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-4">
-                                            <button id="decrement" onclick="stepper(this)">-</button>
-                                            <input type="number" min="0" max="100" step="1"
-                                                value="0" id="my-input" readonly>
-                                            <button id="increment" onclick="stepper(this)">+</button>
-                                        </div>
-                                    </div>
                                 </div>
                             </div>
                         </div>
