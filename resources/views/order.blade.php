3d1b4ddcc9aa5f3e891cd4341f11f66435112863
imas <imasnurdianto2002@gmail.com>
2022-08-31 14:18:41 +0700

    after merge yudis

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -1,83 +1,120 @@
-@extends('Layouts.Main')
+@extends('Layouts.main')
 @section('content')
-
-<div class="page-wrapper pa-0 ma-0 auth-page">
-    <div class="container-fluid">
-        <!-- Row -->
-        <div class="table-struct full-width full-height">
-            <div class="table-cell vertical-align-middle auth-form-wrap">
-                
-                <div class="auth-form  ml-auto mr-auto no-float">
-                    <div class="row">
-                        <div class="checkbox checkbox-success">
-                            <input id="checkbox3" type="checkbox">
-                            <label for="checkbox3"> Success </label>
-                        </div>
-
-                        
-                        <div class="">
-                            
-                            <h6 class=""><strong>Pilih Permainan</strong></h6>
-                        </div>	
-                        <div class="col-sm-12 col-xs-12">
-
-                            <div class="form-wrap">
-                                <form action="#" method="post">
-                                    <div class="form-group">
-                                        <label class="pull-left control-label mb-10" >Data Pengunjung</label>
-                                        <select class="form-control required" id="test" name="form_select" onchange="showDiv(this)">
-                                            <option value="">Pilih metode Pembayaran</option>
-                                            <option value="1">Limit Bulanan</option>
-                                            <option value="2">Limit Kupon</option>
-                                            <option value="3">Cash / Transfer</option>
-                                         </select>
-                                    </div>
-                                    <div class="form-group" id="hidden_div" style="display:none;">
-                                       <input type="radio" name="" value="one game" checked>
-                                       <label class="right control-label mb-10" for="password">One Game</label>
-                                       <label for="">&nbsp;</label>
-                                       <input type="number" id="quantity" name="quantity" min="1" placeholder="0">Praktice     
-                                        <br>
-                                        <input type="number" id="quantity" name="quantity" min="1" placeholder="0">Mobile 1 Cabine
-                                        <input type="number" id="quantity" name="quantity" min="1" placeholder="0">Aditional 1
-                                        <br>
-                                        <input type="number" id="quantity" name="quantity" min="1" placeholder="0">Mobile 2 Cabine
-                                        <input type="number" id="quantity" name="quantity" min="1" placeholder="0">Aditional 2
-                                        <br>
-                                        <input type="number" id="quantity" name="quantity" min="1" placeholder="0">Mobile 3 Cabine
-                                        <input type="number" id="quantity" name="quantity" min="1" placeholder="0">Aditional 3
-                                        <br>
-                                        <button type="submit" class="btn btn-info btn-rounded">Proses</button>
-                                    </div>
-                                    <div class="form-group" id="hidden_div2" style="display:none;">
-                                        <input type="radio" name="" value="one game" checked>
-                                        <label class="control-label mb-10" for="">Pilih Item Tambahan</label>
-                                     </div>
-                                     
-                                    
-                                </form>
+    <div class="page-wrapper">
+        <div class="container-fluid">
+            <div class="row heading-bg">
+                <!-- Breadcrumb -->
+                @include('Layouts.Breadcrumb')
+                <!-- /Breadcrumb -->
+            </div>
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="panel panel-default card-view" style="height: 300px;">
+                        <div class="form-group mt-10 ml-20">
+                            <strong>Pilih Paket Bermain</strong>
+                            <div class="checkbox checkbox-success">
+                                <input type="checkbox" id="cb1">
+                                <label for="cb1">One Games</label>
                             </div>
-                        </div>	
+                            <div class="checkbox checkbox-success">
+                                <input type="checkbox" id="cb2">
+                                <label for="cb2">Practice</label>
+                            </div>
+                        </div>
+                        <div class="col-lg-8">
+                            <div class="form-group">
+                                <strong>Pilih Item Tambahan</strong>
+                                <div class="row">
+                                    <div class="col-lg-4">
+                                        <div class="checkbox checkbox-success">
+                                            <input type="checkbox" id="cb3">
+                                            <label for="cb3">Mobil 1 Cabine</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <div class="checkbox checkbox-success">
+                                            <input type="checkbox" id="cb4">
+                                            <label for="cb4">Caddy</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-4 ">
+                                        <div class="checkbox checkbox-success">
+                                            <input type="checkbox" id="cb5">
+                                            <label for="cb5">Additional 1</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-4">
+                                        <div class="checkbox checkbox-success">
+                                            <input type="checkbox" id="cb6">
+                                            <label for="cb6">Mobil 2 Cabine</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <div class="checkbox checkbox-success">
+                                            <input type="checkbox" id="cb7">
+                                            <label for="cb7">Sewa Tongkat</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-4 ">
+                                        <div class="checkbox checkbox-success">
+                                            <input type="checkbox" id="cb8">
+                                            <label for="cb8">Additional 2</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="panel panel-default card-view" style="height: 300px;">
+                        <h5 style="text-align: center;" class="mt-15 mb-5"><strong>DAFTAR ORDER</strong></h5>
+                        <div class="col-lg-6 mt-5">
+                            <p style="color: #7D7D7D; text-align:left;" class="ml-12">Product</p>
+                        </div>
+                        <div class="col-lg-6 mt-5">
+                            <p style="color: #7D7D7D; text-align:end;">Sub-Total</p>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <hr class="h">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6 mt-5">
+                                <p style="color: #7D7D7D; text-align:start;">One Game</p>
+                            </div>
+                            <div class="col-lg-6 mt-5">
+                                <p style="color: #7D7D7D; text-align:end;">Rp. 1.000.000</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <hr class="h">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6 mt-5">
+                                <p style="text-align:start;"><strong>Total</strong></p>
+                            </div>
+                            <div class="col-lg-6 mt-5">
+                                <p style="text-align:end;">Rp. 1.000.000</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 mt-10">
+                                <div class="btn-proses">
+                                    <a href="/proses">
+                                        <h6 style="color: #ffffff;">Checkout</h6>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- /Row -->	
     </div>
-    
-</div>
-<script>
-    function showDiv(select){
-   if(select.value==1 || select.value==2){
-    document.getElementById('hidden_div').style.display = "block";
-   }
-   else if(select.value==3){
-    document.getElementById('hidden_div').style.display = "block";
-   } else{
-    document.getElementById('hidden_div').style.display = "none";
-   }
-} 
-</script>
-
-
 @endsection
