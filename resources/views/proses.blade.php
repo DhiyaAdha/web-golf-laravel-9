97593000b06e0482ad47dfc14c9df9b74c1042a7
imas <imasnurdianto2002@gmail.com>
2022-09-06 11:29:03 +0700

    proses1

--- resources/views/proses.blade.php
+++ resources/views/proses.blade.php
@@ -26,19 +26,34 @@
                             <strong>
                                 Pilih Paket Bermain
                             </strong>
-
+                            {{-- Radio Button --}}
+                            <div class="pull-right">
+                                <div class="radio radio-success">
+                                    <input type="radio" name="GameType" id="radio1" class="selected-price" value="weekdays" checked>
+                                    <label for="radio1">
+                                        <strong class="text-muted">Price Weekdays</strong>
+                                    </label>
+                                </div>
+                                <br>
+                                <div class="radio radio-success">
+                                    <input type="radio" name="GameType" id="radio2" class="selected-price" value="weekend">
+                                    <label for="radio2">
+                                        <strong class="text-muted">Price Weekends</strong>
+                                    </label>
+                                </div>
+                            </div>
                             @foreach ($default as $item)
                                 <form class="form-inline">
                                     <div class="checkbox checkbox-success">
-                                        <input id="checkbox1" type="checkbox"
-                                            class="form-control form-control-{{ $item->id }}"
-                                            onchange="valueChanged({{ $item->id }})">
+                                        <input id="checkbox" type="checkbox"
+                                            class="form-control select-item form-control-{{ $item->id }}"
+                                            onchange="valueChanged({{ $item->id }})" data-name="{{ $item->name }}" data-priceday="{{ $item->price_weekdays }}" data-priceend="{{ $item->price_weekend }}">
                                         <label for=""></label>
                                     </div>
                                     <span class="wrap-quantity-{{ $item->id }}" style="display: none">
                                         <button type="button" id="decrement"
                                             onclick="stepper('decrement', {{ $item->id }})">-</button>
-                                        <input type="number" min="1" max="10" step="1" value="1"
+                                        <input name="input1" type="number" min="0" max="100" step="1" value="0"
                                             id="my-input-{{ $item->id }}" readonly>
                                         <button type="button" id="increment"
                                             onclick="stepper('increment', {{ $item->id }})">+</button>
@@ -51,25 +66,29 @@
                             <div class="form-group">
                                 <strong>Pilih Item Tambahan</strong>
                                 <div class="row">
+                                    <form class="form-inline">
                                     @foreach ($additional as $item2)
-                                        <form class="form-inline">
+                                    <div>
                                             <div class="checkbox checkbox-success">
+                                                <div class="row">
                                                 <input id="checkbox2" type="checkbox"
-                                                    class="form-control form-control-{{ $item2->id }}"
-                                                    onchange="valueChanged({{ $item2->id }})">
+                                                    class="form-control select-item form-control-{{ $item2->id }}"
+                                                    onchange="valueChanged({{ $item2->id }})" data-name="{{ $item2->name }}" data-priceday="{{ $item2->price_weekdays }}" data-priceend="{{ $item2->price_weekend }}">
                                                 <label for=""></label>
+                                                </div>
                                             </div>
                                             <span class="wrap-quantity-{{ $item2->id }}" style="display: none">
-                                                <button type="button" id="decrement"
+                                                <button id="decrement"
                                                     onclick="stepper('decrement', {{ $item2->id }})">-</button>
-                                                <input type="number" min="1" max="10" step="1"
-                                                    value="1" id="my-input-{{ $item2->id }}" readonly>
-                                                <button type="button" id="increment"
+                                                <input name="input2" type="number" min="0" max="100" step="1"
+                                                    value="0" id="my-input-{{ $item2->id }}" readonly>
+                                                <button id="increment"
                                                     onclick="stepper('increment', {{ $item2->id }})">+</button>
                                             </span>
                                             <label for="">{{ $item2->name }}</label>
+                                    </div>
+                                            @endforeach
                                         </form>
-                                    @endforeach
                                 </div>
                             </div>
                         </div>
@@ -89,7 +108,10 @@
                                 <hr class="h">
                             </div>
                         </div>
-                        <div class="row">
+                        <div class="wrap-selected-item">
+                        </div>
+                        {{-- <div class="row">
+                            
                             <div class="col-lg-6 mt-5">
                                 @foreach ($default as $item)
                                     <p style="color: #7D7D7D; text-align:start;">
@@ -120,7 +142,7 @@
                                     </p>
                                 @endforeach
                             </div>
-                        </div>
+                        </div> --}}
                         <div class="row">
                             <div class="col-lg-12">
                                 <hr class="h">
@@ -131,7 +153,8 @@
                                 <p style="text-align:start;"><strong>Total</strong></p>
                             </div>
                             <div class="col-lg-6 mt-5">
-                                <p style="text-align:end;">Rp. 1.000.000,00</p>
+                                <p style="text-align:end;"></p>
+                                {{-- <p style="text-align:end;">Rp. 1.000.000,00</p> --}}
                             </div>
                         </div>
                         <div class="row">
@@ -148,7 +171,6 @@
             </div>
         </div>
     </div>
-@endsection
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.0/jquery.min.js"></script>
 <script type="text/javascript">
     function valueChanged(id) {
@@ -158,13 +180,35 @@
             $(".wrap-quantity-" + id).hide();
     }
 
-    $(document).ready(function() {
-        $('#isChecked').change(function() {
-            if ($(this.checked).length) {
-                $(".wrap-txt-" + id).show();
-            } else {
-                $(".wrap-txt-" + id).hide();
-            }
-        });
-    });
+    // $("#increment").click(function() {
+    //     alert("The Form has been Submitted.");
+    // });
+
+    // $(document).ready(function() {
+    //     $('#isChecked').change(function() {
+    //         if ($(this.checked).length) {
+    //             $(".wrap-txt-" + id).show();
+    //         } else {
+    //             $(".wrap-txt-" + id).hide();
+    //         }
+    //     });
+    // });
+    // $(document).on('click', '.select-item', function () {
+    //     var radio = $(".selected-price:checked").val();
+    //     var name = $(this).data('name');
+    //     console.log(radio)
+    //     var price = (radio == 'weekdays') ? $(this).data('priceday') : $(this).data('priceend');
+        
+    //     var html = `<div class="row">
+    //                             <div class="col-lg-6 mt-5">
+    //                                 <p style="color: #7D7D7D; text-align:start;">${name}</p>
+    //                             </div>
+    //                             <div class="col-lg-6 mt-5">
+                                
+    //                                 <p style="color: #7D7D7D; text-align:end;">RP ${price}</p>
+    //                             </div>
+    //                         </div>`;
+    //     $('.wrap-selected-item').append(html)
+    // })
 </script>
+@endsection
