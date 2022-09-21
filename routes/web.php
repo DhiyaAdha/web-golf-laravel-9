2aa7ad7b5380b285ef1fc971f1495f2ef313e2fc
Dhiya <adiadiadha@google.com>
2022-09-21 09:44:01 +0700

    up dhiya daftar tamu

--- routes/web.php
+++ routes/web.php
@@ -102,6 +102,7 @@ Route::group(['middleware' => ['auth', 'ceklevel:1,2']], function () {
     Route::get('reportlimit/{id}', [TamuController::class, 'reportlimit'])->name('limit.report.data');
     Route::get('reporttransaksi/{id}', [TamuController::class, 'reporttransaksi'])->name('transaksi.report.data');
     Route::post('/tambah-deposit', [TamuController::class, 'insertdeposit'])->name('insertdeposit');
+    route::get('/export_excel_tamu', [TamuController::class, 'export_excel_tamu'])->name('export_excel_tamu');
     Route::get('/proses', [OrderController::class, 'index'])->name('proses');
     Route::get('/cart/{id}', [OrderController::class, 'index'])->name('order.cart');
     Route::resource('cart', OrderController::class);
@@ -109,6 +110,5 @@ Route::group(['middleware' => ['auth', 'ceklevel:1,2']], function () {
     Route::get('/cart/remove/{package}',[OrderController::class, 'remove'])->name('cart.remove');
     Route::get('/checkout', [OrderController::class,'checkout'])->name('checkout');
     
-    route::get('/export_excel_tamu', [TamuController::class, 'export_excel_tamu'])->name('export_excel_tamu');
 
 });
