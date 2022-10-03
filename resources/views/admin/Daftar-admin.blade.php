ddce7f03877250cfb53a68d3d7ab5f3504c55df8
Dhiya <adiadiadha@google.com>
2022-10-03 15:33:46 +0700

    up d

--- resources/views/admin/Daftar-admin.blade.php
+++ resources/views/admin/Daftar-admin.blade.php
@@ -23,7 +23,7 @@
                     <div class="panel panel-default card-view">
                         <div class="panel-heading">
                             <div class="pull-left">
-                                <h6 class="panel-title txt-dark">Daftar Paket</h6>
+                                <h6 class="panel-title txt-dark">Daftar Admin</h6>
                             </div>
                             <div class="pull-right">
                                 <a href="{{ route('tambah-admin') }}" class="btn btn-xs btn-success">Tambah
