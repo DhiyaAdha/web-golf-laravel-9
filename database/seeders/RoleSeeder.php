c42e674697fb5adba629fe7c39263ee1aa103fa5
kevin <setiawank417@gmail.com>
2022-10-18 11:59:01 +0700

    19-okt-2022

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,11 +16,9 @@ class RoleSeeder extends Seeder
     {
         
         Role::create([
-            
             'id' => '1',
             'name' => 'Admin',
         ]);
-
         Role::create([
             
             'id' => '2',
