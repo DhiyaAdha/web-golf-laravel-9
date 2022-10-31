9d440cade174a831c2a2924d3ce357d005f40b16
imas <imasnurdianto2002@gmail.com>
2022-10-31 10:43:20 +0700

    pull dhiya1

--- resources/views/dashboard/revenue.blade.php
+++ resources/views/dashboard/revenue.blade.php
@@ -124,7 +124,7 @@
                             <div class="pull-right">
                                 <div class="dropdown show">
                                     <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: black">
-                                      Dropdown link
+                                      Dropdown Link
                                     </a>
                                   
                                     <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
