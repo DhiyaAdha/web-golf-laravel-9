cb8f5d105c23f1a41cabc09fd358ccbaec89399c
Zaenury Dhany Wibowo <zaenury5868@gmail.com>
2022-11-02 16:09:58 +0700

    merge ima

--- resources/views/tamu/daftar-tamu.blade.php
+++ resources/views/tamu/daftar-tamu.blade.php
@@ -1,26 +1,20 @@
 @extends('layouts.main', ['title' => 'TGCC | Daftar Tamu'])
 @section('content')
-    <!-- Main Content -->
     <div class="page-wrapper">
         <div class="container-fluid">
-            <!-- Title -->
             <div class="row heading-bg">
                 <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
                     <h5 class="txt-dark">Daftar Tamu</h5>
                 </div>
-                <!-- Breadcrumb -->
                 <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                     <ol class="breadcrumb">
                         <li><a href="{{ url('analisis-tamu') }}">Dashboard</a></li>
                         <li class="active"><span>Daftar Tamu</span></li>
                     </ol>
                 </div>
-                <!-- /Breadcrumb -->
             </div>
-            <!-- /Title -->
 
             <div class="row">
-                <!-- Basic Table -->
                 <div class="col-sm-12">
                     <div class="panel panel-default card-view">
                         <div class="panel-heading">
@@ -94,7 +88,6 @@
                         </div>
                     </div>
                 </div>
-                <!-- /Basic Table -->
             </div>
             @include('layouts.footer')
         </div>
