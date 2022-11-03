d763c0cf07ab432cc0e88073a9214c4d50641c12
imas <imasnurdianto2002@gmail.com>
2022-11-03 15:05:38 +0700

    push sore 03/11/2022

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -1,58 +1,16 @@
-<!-- jQuery -->
-
-
-<!-- Bootstrap Core JavaScript -->
 <script src="{{ asset('vendors/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-
-<!-- Data table JavaScript -->
 <script src="{{ asset('vendors/bower_components/datatables/media/js/jquery.dataTables.min.js') }}"></script>
-
-<!-- Slimscroll JavaScript -->
 <script src="{{ asset('dist/js/jquery.slimscroll.js') }}"></script>
-
-<!-- simpleWeather JavaScript -->
 <script src="{{ asset('vendors/bower_components/moment/min/moment.min.js') }}"></script>
-<script src="{{ asset('vendors/bower_components/simpleWeather/jquery.simpleWeather.min.js') }}"></script>
-<script src="{{ asset('dist/js/simpleweather-data.js') }}"></script>
-
-<!-- Progressbar Animation JavaScript -->
 <script src="{{ asset('vendors/bower_components/waypoints/lib/jquery.waypoints.min.js') }}"></script>
 <script src="{{ asset('vendors/bower_components/jquery.counterup/jquery.counterup.min.js') }}"></script>
-
-<!-- Fancy Dropdown JS -->
-<script src="{{ asset('dist/js/dropdown-bootstrap-extended.js') }}"></script>
-
-<!-- Owl JavaScript -->
-<script src="{{ asset('vendors/bower_components/owl.carousel/dist/owl.carousel.min.js') }}"></script>
-
-<script src="{{ asset('vendors/bower_components/bootstrap-switch/dist/js/bootstrap-switch.min.js') }}"></script>
-
-<!-- ChartJS JavaScript -->
-<script src="{{ asset('vendors/chart.js/Chart.min.js') }}"></script>
-
-<!-- Bootstrap Switches CSS -->
-<script src="{{ asset('vendors/bower_components/bootstrap-switch/dist/js/bootstrap-switch.js') }}"></script>
-
-
-<!-- Morris Charts JavaScript -->
-<script src="{{ asset('vendors/bower_components/raphael/raphael.min.js') }}"></script>
-<script src="{{ asset('vendors/bower_components/morris.js/morris.min.js') }}"></script>
 <script src="{{ asset('vendors/bower_components/jquery-toast-plugin/dist/jquery.toast.min.js') }}"></script>
-
-<!-- Switchery JavaScript -->
-<script src="{{ asset('vendors/bower_components/switchery/dist/switchery.min.js') }}"></script>
 <script src="{{ asset('vendors/bower_components/sweetalert/dist/sweetalert.min.js') }}"></script>
-<script src="{{ asset('vendors/bower_components/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js') }}">
-</script>
-
-<!-- Init JavaScript -->
 <script src="{{ asset('/dist/js/init.js') }}"></script>
+<script src="https://kit.fontawesome.com/cc01c97c5b.js" crossorigin="anonymous"></script>
 @stack('scripts')
 <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
 <script type="text/javascript" src="{{ asset('/dist/js/printThis.js') }}"></script>
-
-{{-- Font Awesome --}}
-<script src="https://kit.fontawesome.com/cc01c97c5b.js" crossorigin="anonymous"></script>
 <script>
     $( document ).ajaxComplete(function() {
         $('[data-toggle="tooltip"]').tooltip({
@@ -86,50 +44,4 @@
     $(function() {
         $('[data-toogle="tooltip"]').tooltip()
     })
-    // kartu-tamu(transaksi)
-    $('#transaksi').DataTable({
-        "processing": true,
-        "serverSide": true,
-        "lengthChange": false,
-        "searching": true,
-        "paginate": {
-            "first": "First",
-            "last": "Last",
-            "next": "Next",
-            "previous": "Previous"
-        },
-        "ajax": {
-            "url": "{{ route('kartu-tamu') }}",
-            "type": "GET",
-            "datatype": "json"
-        },
-        "render": $.fn.dataTable.render.text(),
-        "columns": [{
-                data: 'id',
-                searchable: true,
-                orderable: false
-            },
-            {
-                data: 'status',
-                searchable: true,
-                orderable: false
-            },
-        ],
-        order: [],
-        responsive: true,
-        language: {
-            search: "",
-            searchPlaceholder: "Cari",
-            emptyTable: "Tidak ada data pada tabel ini",
-            info: "Menampilkan _START_ s/d _END_ dari _TOTAL_ data",
-            infoFiltered: "(difilter dari _MAX_ total data)",
-            infoEmpty: "Tidak ada data pada tabel ini",
-            lengthMenu: "Menampilkan _MENU_ data",
-            zeroRecords: "Tidak ada data pada tabel ini"
-        },
-        columnDefs: [{
-            className: 'text-left',
-            targets: [1, 2, 3, ]
-        }],
-    });
 </script>
