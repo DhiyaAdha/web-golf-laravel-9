9149e6af9c2010c41ca75c5cbe8d19689ba1b6e7
imas <imasnurdianto2002@gmail.com>
2022-11-22 14:04:32 +0700

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -61,7 +61,7 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/')->with('success', 'Anda Berhasil Logout');
+        return redirect('/login')->with('success', 'Anda Berhasil Logout');
     }
 
     public function resetPassword(Request $request)
