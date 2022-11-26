ba1c12a7ea41ee32cae62d1e9aa847b861cdfb99
Zaenury Dhany Wibowo <zaenury5868@gmail.com>
2022-11-26 21:54:03 +0700

    disabled forgot password

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -165,7 +165,7 @@
                                                 @enderror
                                             </div>
                                             <div class="form-group">
-                                                <a class="capitalize-font txt-primary block mb-10 pull-right font-12" href="{{ route('forgot-password') }}">Lupa Password?</a>
+                                                {{-- <a class="capitalize-font txt-primary block mb-10 pull-right font-12" href="{{ route('forgot-password') }}">Lupa Password?</a> --}}
                                                 <label class="pull-left control-label mb-10" for="password">Password</label>
                                                 <input type="password" name="password" class="form-control" id="password" placeholder="Masukan password" required>
                                                 <i style="color: rgb(114, 114, 114);" class="fa-solid fa-eye fa-eye-slash" id="eye"></i>
