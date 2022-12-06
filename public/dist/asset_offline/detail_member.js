2c4d1814a25e43070e7d724e8097fd46f5b211fc
Zaenury Dhany Wibowo <zaenury5868@gmail.com>
2022-12-06 17:18:14 +0700

    jsdffhjdsjkfds

--- public/dist/asset_offline/detail_member.js
+++ public/dist/asset_offline/detail_member.js
@@ -64,13 +64,45 @@ $(".modal-detail-invoice").on("show.bs.modal", function (e) {
     });
 });
 
-$(".download-kartu-tamu").on("click", function () {
-    $(".resolution").printThis({
-        importCSS: true,
-        importStylse: true,
-        loadCSS: "dist/css/custom.css",
-        header: "<h6>Kartu Member</h6>",
+function screenshot(){
+    html2canvas(document.getElementById("resolution")).then(function(canvas){
+        downloadImage(canvas.toDataURL(),"UsersInformation.png");
     });
+}
+
+function downloadImage(uri, filename){
+    var link = document.createElement('a');
+    if(typeof link.download !== 'string'){
+        window.open(uri);
+    }
+    else{
+        link.href = uri;
+        link.download = filename;
+        accountForFirefox(clickLink, link);
+    }
+}
+
+function clickLink(link){
+    link.click();
+}
+
+function accountForFirefox(click){
+    var link = arguments[1];
+    document.body.appendChild(link);
+    click(link);
+    document.body.removeChild(link);
+}
+
+$(".download-kartu-tamu").on("click", function () {
+    $('#kartu-tamu').modal('hide');
+    screenshot();
+    
+    // $(".resolution").printThis({
+    //     importCSS: true,
+    //     importStylse: true,
+    //     loadCSS: "dist/css/custom.css",
+    //     header: "<h6>Kartu Member</h6>",
+    // });
 
     // const fullName = $(this).data('name');
     // const [first, last] = fullName.split(' ');
