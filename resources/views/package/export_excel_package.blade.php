fb2e2d2a8ac0375e151b06ce89062c6353d1da65
yudistira-rk <yudistiraeducation100@gmail.com>
2022-12-07 09:52:06 +0700

    07/12/2022 ubah nama kategori pada saat export excel

--- resources/views/package/export_excel_package.blade.php
+++ resources/views/package/export_excel_package.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document Daftar Tamu</title>
 </head>
+
 <body>
     <table>
         <thead>
@@ -19,20 +21,34 @@
             </tr>
         </thead>
         <tbody>
-        @foreach($package as $item)
-            <tr>
-                <td>{{ $item->name }}</td>
-                <td>{{ $item->category }}</td>
-                <td>Rp.{{ number_format($item->price_discount, 0, ',', '.') }}</td>
-                <td>Rp.{{ number_format($item->price_weekdays, 0, ',', '.') }}</td>
-                <td>Rp.{{ number_format($item->price_weekend, 0, ',', '.') }}</td>
-                @if ($item->status == 0)
-                    <td>Aktif</td>
-                @else
-                    <td>Tidak Aktif</td>
-                @endif
-        @endforeach
+            {{-- {{ $item->category }} --}}
+            @foreach ($package as $item)
+                <tr>
+                    <td>{{ $item->name }}</td>
+                    <td>
+                        @if ($item->category == 'default')
+                        {{ "Permainan" }}
+                        @elseif ($item->category == 'additional')
+                        {{ "Proshop & Fasilitas" }}
+                        @elseif ($item->category == 'others')
+                        {{ "Kantin" }}
+                        @elseif ($item->category == 'rental')
+                        {{ "Sewa" }}
+                        @elseif ($item->category == 'service')
+                        {{ "Service Fee" }}
+                        @endif
+                    </td>
+                    <td>Rp.{{ number_format($item->price_discount, 0, ',', '.') }}</td>
+                    <td>Rp.{{ number_format($item->price_weekdays, 0, ',', '.') }}</td>
+                    <td>Rp.{{ number_format($item->price_weekend, 0, ',', '.') }}</td>
+                    @if ($item->status == 0)
+                        <td>Aktif</td>
+                    @else
+                        <td>Tidak Aktif</td>
+                    @endif
+            @endforeach
         </tbody>
     </table>
 </body>
+
 </html>
