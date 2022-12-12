3b88721d9c66c3c3a7d71d3a7a55f8049450c0ba
daenuli <giarsyani.nuli@gmail.com>
2022-12-12 09:33:08 +0700

    update export transaksi

--- resources/views/dashboard/export.blade.php
+++ resources/views/dashboard/export.blade.php
@@ -1,14 +1,41 @@
 <table>
     <thead>
-    <tr>
-        <th>No {{ $category }}</th>
-        <th>Invoice</th>
-        <th>Tanggal</th>
-        <th>Nama Barang</th>
-        <th>Jumlah Barang</th>
-        <th>Harga Barang</th>
-        <th>Total Nilai</th>
-    </tr>
+        <tr>
+            <th>No</th>
+            @if($category == 'default')
+                <th>Invoice</th>
+                <th>Tanggal</th>
+                <th>Nama</th>
+                <th>Paket Permainan</th>
+                <th>Total Paket</th>
+                <th>Total Nilai</th>
+            @elseif($category == 'additional')
+                <th>Invoice</th>
+                <th>Tanggal</th>
+                <th>Nama Barang</th>
+                <th>Jumlah Barang</th>
+                <th>Harga Barang</th>
+                <th>Total Nilai</th>
+            @elseif($category == 'others')
+                <th>Tanggal</th>
+                <th>Nama Produk</th>
+                <th>Jumlah Produk</th>
+                <th>Harga Produk</th>
+                <th>Total Nilai</th>
+            @elseif($category == 'service')
+                <th>Tanggal</th>
+                <th>Invoice</th>
+                <th>Nama Tamu</th>
+                <th>Total Nilai Fee</th>
+            @elseif($category == 'rental')
+                <th>Tanggal</th>
+                <th>Invoice</th>
+                <th>Nama Barang</th>
+                <th>Jumlah Barang</th>
+                <th>Harga Barang</th>
+                <th>Total Nilai</th>
+            @endif
+        </tr>
     </thead>
     <tbody>
     @php
@@ -17,12 +44,39 @@
     @foreach($data as $i => $row)
         <tr>
             <td>{{ $no++ }}</td>
-            <td>{{ $row['inv'] }}</td>
-            <td>{{ $row['date'] }}</td>
-            <td>{{ $row['product'] }}</td>
-            <td>{{ $row['qty'] }}</td>
-            <td>{{ $row['pricesingle'] }}</td>
-            <td>{{ $row['price'] }}</td>
+            @if($category == 'default')
+                <td>{{ $row['inv'] }}</td>
+                <td>{{ $row['date'] }}</td>
+                <td>{{ $row['username'] }}</td>
+                <td>{{ $row['product'] }}</td>
+                <td>{{ $row['qty'] }}</td>
+                <td>{{ $row['price'] }}</td>
+            @elseif($category == 'additional')
+                <td>{{ $row['inv'] }}</td>
+                <td>{{ $row['date'] }}</td>
+                <td>{{ $row['product'] }}</td>
+                <td>{{ $row['qty'] }}</td>
+                <td>{{ $row['pricesingle'] }}</td>
+                <td>{{ $row['price'] }}</td>
+            @elseif($category == 'others')
+                <td>{{ $row['date'] }}</td>
+                <td>{{ $row['product'] }}</td>
+                <td>{{ $row['qty'] }}</td>
+                <td>{{ $row['pricesingle'] }}</td>
+                <td>{{ $row['price'] }}</td>
+            @elseif($category == 'service')
+                <td>{{ $row['date'] }}</td>
+                <td>{{ $row['inv'] }}</td>
+                <td>{{ $row['username'] }}</td>
+                <td>{{ $row['price'] }}</td>
+            @elseif($category == 'rental')
+                <td>{{ $row['date'] }}</td>
+                <td>{{ $row['inv'] }}</td>
+                <td>{{ $row['product'] }}</td>
+                <td>{{ $row['qty'] }}</td>
+                <td>{{ $row['pricesingle'] }}</td>
+                <td>{{ $row['price'] }}</td>
+            @endif
         </tr>
     @endforeach
     </tbody>
